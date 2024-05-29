2ed672697ecb923ec7916a9bd28c5091dd7e39b6
prestonm44 <preston.mitchell4444@gmail.com>
2024-05-28 21:19:50 -0600

    finished assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
 import playerData from './playerData.js';
+import { useState } from 'react';
 
 function BaseballCard(props) {
-  // return (
-  //   <div className="card">
-  //     <h2>{props.name}</h2>
-  //     <img src={props.imgUrl} alt={props.name}/>
-  //   </div>
-  // );
-  const statsDisplay = [];
-  for stat 
+  const [showPicture, setShowPicture] = useState(true);
+
+  const toggleCard = () => {
+    setShowPicture(!showPicture);
+  }
+  if (showPicture) {
   return (
-    <div className='card'>
+    <div className="card" onClick={toggleCard}>
       <h2>{props.name}</h2>
-      <p>{props.position}</p>
+      <img src={props.imgUrl} alt={props.name}/>
+    </div>
+  );
+} else {
+  const statsDisplay = Object.entries(props.stats).map(([statName, statValue]) => (
+    <p key={statName}>
+      {statName}: {statValue}
+    </p>
+  ));
+  return (
+    <div className='card' onClick={toggleCard}>
+      <h2>{props.name}</h2>
+      <p>Position: {props.team}</p>
+      <p>Team: {props.position}</p>
+      <p>{statsDisplay}</p>
     </div>
   )
+}
 }
 
 function App() {
